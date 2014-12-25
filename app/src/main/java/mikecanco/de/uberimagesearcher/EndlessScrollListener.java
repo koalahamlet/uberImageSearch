f9d279d2f4d6892f7be3d7b9102a034d8f8ed633
koalahamlet <koalahamlet@gmail.com>
2014-12-25 09:50:58 -0800

    fix short result bug

--- app/src/main/java/mikecanco/de/uberimagesearcher/EndlessScrollListener.java
+++ app/src/main/java/mikecanco/de/uberimagesearcher/EndlessScrollListener.java
@@ -39,40 +39,34 @@ public abstract class EndlessScrollListener implements AbsListView.OnScrollListe
     {
         // If the total item count is zero and the previous isn't, assume the
         // list is invalidated and should be reset back to initial state
-        // If there are no items in the list, assume that initial items are loading
-        if (!loading && (totalItemCount < previousTotalItemCount)) {
+        if (totalItemCount < previousTotalItemCount) {
             this.currentPage = this.startingPageIndex;
             this.previousTotalItemCount = totalItemCount;
             if (totalItemCount == 0) { this.loading = true; }
         }
-
-        // If its still loading, we check to see if the dataset count has
+        // If it’s still loading, we check to see if the dataset count has
         // changed, if so we conclude it has finished loading and update the current page
         // number and total item count.
-        if (loading) {
-            if (totalItemCount > previousTotalItemCount) {
-                loading = false;
-                previousTotalItemCount = totalItemCount;
-                currentPage++;
-            }
+        if (loading && (totalItemCount > previousTotalItemCount)) {
+            loading = false;
+            previousTotalItemCount = totalItemCount;
+            currentPage++;
         }
 
-        // If it isnt currently loading, we check to see if we have breached
+        // If it isn’t currently loading, we check to see if we have breached
         // the visibleThreshold and need to reload more data.
         // If we do need to reload some more data, we execute onLoadMore to fetch the data.
-        if (!loading && (totalItemCount - visibleItemCount)<=(firstVisibleItem + visibleThreshold))
-        {
+        if (!loading && (totalItemCount - visibleItemCount)<=(firstVisibleItem + visibleThreshold)) {
             onLoadMore(currentPage + 1, totalItemCount);
             loading = true;
         }
     }
 
     // Defines the process for actually loading more data based on page
-    public abstract  void onLoadMore(int page, int totalItemsCount);
+    public abstract void onLoadMore(int page, int totalItemsCount);
 
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
         // Don't take any action on changed
     }
-
 }
